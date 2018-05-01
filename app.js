a00408f6c0fd846dc1e1dd6631cbc2dd589ece3d
sanje <sanjeeda_noor@yahoo.com>
2018-05-01 23:20:34 +0600

    Initial commit

--- app.js
+++ app.js
@@ -2,20 +2,55 @@ const	express=require("express"),
 		app=express(),
 	
 		mongoose=require("mongoose"),
+		bodyParser=require("body-parser"),
+		ejsLint = require('ejs-lint'),
+		//AUTHENTICATION PACKAGE
+		passport=require("passport"),
+		LocalStrategy=require("passport-local"),
 		
 		//IMPORT MODELS
 		User=require("./models/user"),
 		Transaction=require("./models/transaction"),
 		Invoice=require("./models/invoice"),
-		
+		common="123",
 		//IMPORT ROUTES
 		userRoutes=require("./routes/userRoute");
 
 		mongoose.connect("mongodb://localhost/wallet-first");
 
-app.use(express.static("public"));
-app.use(userRoutes);
+
 app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+ejsLint("user");
+//app.use(ejsLint("user"));//NOT WORKING ERROR: app.use() requires a middleware function
+
+
+
+//ENABLE AUTHENTICATION
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(require("express-session")(
+{
+secret:"BeHappy", 
+resave:false,
+saveUninitiazed:false
+}
+
+))
+app.use(userRoutes);
+app.use(function(req, res, next){
+	res.locals.currentUser=req.user;
+	
+	next();
+});
+
+//END OF AUTHENTICATION
 
 var userArray=[
 	{
@@ -36,5 +71,5 @@ var userArray=[
 */
 
 app.listen(3000, function(){
-	console.log("survived");
+	console.log("champ");
 });
